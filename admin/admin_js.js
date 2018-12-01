bb8f49d71a718bed78d6ba28da7ed8bcbdd5ff69
thehao159 <44767198+thehao159@users.noreply.github.com>
2018-12-01 10:54:45 +0700

    Add files via upload

--- admin/admin_js.js
+++ admin/admin_js.js
@@ -27,9 +27,55 @@ function openproducts()
 				<td>`+i.price+`</td>
 				<td>&times</td>
 				<td>&Theta;</td>
-			 </tr>`;
+			 	</tr>`;
 	}
 	document.getElementById("the_lists").innerHTML = s;
+}
+
+function searchproducts(list,ten)
+{
+	var t = list;
+	var findedproducts = [];
+	for (var i of t)
+	{
+		if (i.name.toUpperCase().indexOf(ten.toUpperCase()) != -1)
+		{
+			findedproducts.push(i);
+		}
+	}
+	return findedproducts;
+}
+
+function checkproducts()
+{	
+	var t = getlocalstorage();
+	var ten = document.getElementById("search").value;
+	var s = `<tr>
+				<td>STT</td>
+				<td>Tên</td>
+				<td>Giá</td>
+				<td>Xóa</td>
+				<td>Sửa</td>
+			 </tr>	`;
+	var n = 0;
+	var l = searchproducts(t,ten);
+	for (var i of l)
+	{
+		n++;
+		s += `<tr>
+				<td>`+n+`</td>
+				<td><a href="#">`+i.name+`<a></td>
+				<td>`+i.price+`</td>
+				<td>&times</td>
+				<td>&Theta;</td>
+			 </tr>`;
+	}
+	if (n!=0) document.getElementById("the_lists").innerHTML = s;
+	else
+	{
+		s += `<td colspan="4"><i>Không Tìm Thấy Kết Quả</i></td>`;
+		document.getElementById("the_lists").innerHTML = s;
+	}
 }
 
 function closeproducts()
